574fcd57f1ced7061fb4cffcbeea4b90d834f152
Muxammadamin Ulmasaliyev <ulmasaliyev2005@gmail.com>
2024-08-16 23:25:29 +0500

    #fixed DB connection issues

--- RouteRunner/App.xaml.cs
+++ RouteRunner/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using RouteRunnerLibrary;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -15,6 +17,25 @@ namespace RouteRunner
 			SQLitePCL.Batteries.Init();
 
 			// Your code here
+
+			AppDomain.CurrentDomain.UnhandledException += GlobalExceptionHandler;
+
+
+			using (var context = new AppDbContext())
+			{
+				context.Database.Migrate();
+			}
+
+		}
+		private void GlobalExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+		{
+			Exception exception = (Exception)e.ExceptionObject;
+
+			// Show error message in a MessageBox
+			MessageBox.Show($"An unhandled exception occurred: {exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+			// Optionally log the exception or perform other actions
+
 		}
 	}
 
--- RouteRunnerLibrary/AppDbContext.cs
+++ RouteRunnerLibrary/AppDbContext.cs
@@ -8,17 +8,24 @@ public class AppDbContext : DbContext
 	public DbSet<Folder> Folders { get; set; }
 	public DbSet<SavedRequest> SavedRequests { get; set; }
 
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)  
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
-		string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+		/*string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 		string routeRunnerFolderPath = Path.Combine(documentsPath, "RouteRunner");
 
 		if (!Directory.Exists(routeRunnerFolderPath))
 		{
 			Directory.CreateDirectory(routeRunnerFolderPath);
 		}
+
 		string dbPath = Path.Combine(routeRunnerFolderPath, "route-runner-app.db");
+
+		optionsBuilder.UseSqlite($"Data Source={dbPath}");*/
+
+
+		string dbPath = Path.Combine(AppContext.BaseDirectory, "route-runner-app.db");
 		optionsBuilder.UseSqlite($"Data Source={dbPath}");
+
 
 	}
 
